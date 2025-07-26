c7f12676cfa41a67cf5c7d4c41504b98b76cb1a1
nagclk <nagclk@gmail.com>
2025-07-26 15:55:34 +0530

    Update main.tf

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -10,6 +10,6 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "resourcegroup-test-tbd-3"
-  location = "westeurope"
+  name     = "tfstate-rg"
+  location = "southindia"
 }
